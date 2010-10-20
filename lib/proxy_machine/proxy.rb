bc08b7dff0e19461ae2e64d2df533327e7f4f1e4
tulios <ornelas.tulio@gmail.com>
2010-10-20 17:41:23 -0200

    awesome features and updating the readme

--- lib/proxy_machine/proxy.rb
+++ lib/proxy_machine/proxy.rb
@@ -2,30 +2,37 @@ module ProxyMachine
   
   class Proxy < BasicObject
 
-    def initialize object, calls = nil
+    def initialize object, params = nil
       @object = object                    
       @before = @before_all = @after = @after_all = nil
+      @allow_dinamic = false
+      @avoid_original_execution = false
       
-      if calls                  
-        @before = calls[:before]
-        @before_all = calls[:before_all]
-        @after = calls[:after]
-        @after_all = calls[:after_all]
+      if params
+        @allow_dinamic = params[:allow_dinamic]
+        @avoid_original_execution = params[:avoid_original_execution]
+        @before = params[:before]
+        @before_all = params[:before_all]
+        @after = params[:after]
+        @after_all = params[:after_all]
       end
     end
                             
     def method_missing(symbol, *args)
       @symbol = symbol        
       method = symbol.to_s
-      raise NoMethodError.new(method) unless @object.methods.member?(method)
+      
+      unless @allow_dinamic
+        raise NoMethodError.new(method) unless @object.methods.member?(method)
+      end
                                        
-      execute_call(@before_all, @object)
+      execute_call(@before_all, @object, symbol, args)
       execute_call(@before, @object)
-
-      @result = @object.send(method, *args)
+                              
+      @result = @avoid_original_execution ? nil : @object.send(method, *args)
 
       result_after = execute_call(@after, @object, @result)
-      result_after_all = execute_call(@after_all, @object, @result)
+      result_after_all = execute_call(@after_all, @object, @result, symbol, args)
          
       return result_after_all if result_after_all
       return result_after if result_after
@@ -33,6 +40,7 @@ module ProxyMachine
     end
     
     def proxied_class?; true end
+    def original_object; @object end
     
     private               
     def execute_call container, *args
--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -54,21 +54,38 @@ describe Proxy do
   end
   
   context 'for all methods' do
+    class AllMethodsClass
+      attr_accessor :method, :args
+      def do_something; 50 end
+    end             
     
     it 'should add a callbefore' do
       array = [3, 2, 1]
-      proxy = Proxy.new array, :before_all => lambda {|obj| obj.sort!}
+      proxy = Proxy.new array, :before_all => lambda {|obj, method, args| obj.sort!}
       proxy.should_not be_nil
       proxy.reverse.should == [3, 2, 1]
       proxy.max.should == 3
       proxy.first.should == 1
       proxy.to_s.should == "123"
     end
+    
+    it 'should provide method name and arguments for the block in callbefore' do
+      obj = AllMethodsClass.new
+      proxy = proxy_for obj, :before_all => lambda {|obj, method, args|
+        obj.method = method
+        obj.args = args
+      }
+      
+      proxy.should_not be_nil
+      proxy.do_something.should == 50
+      proxy.original_object.method.should == :do_something
+      proxy.original_object.args.should be_empty
+    end
 
     it 'should add a callafter' do
       array = [1, 2, 3]
       
-      proxy = Proxy.new array, :after_all => lambda {|obj, result|
+      proxy = Proxy.new array, :after_all => lambda {|obj, result, method, args|
         return result * 10 if result.class == Fixnum
         result
       }
@@ -80,11 +97,25 @@ describe Proxy do
       proxy.to_s.should == "123"
     end
     
+    it 'should provide method name and arguments for the block in callafter' do
+      obj = AllMethodsClass.new
+      proxy = proxy_for obj, :after_all => lambda {|obj, result, method, args|
+        obj.method = method
+        obj.args = args
+        nil
+      }
+      
+      proxy.should_not be_nil
+      proxy.do_something.should == 50
+      proxy.original_object.method.should == :do_something
+      proxy.original_object.args.should be_empty
+    end
+    
     it 'should add both, callbefore and callafter' do
       array = [3, 2, 1]
       proxy = Proxy.new array, 
-      :before_all => lambda {|obj| obj.sort!},
-      :after_all => lambda {|obj, result|
+      :before_all => lambda {|obj, method, args| obj.sort!},
+      :after_all => lambda {|obj, result, method, args|
         return result * 10 if result.class == Fixnum
         result
       }
@@ -134,8 +165,8 @@ describe Proxy do
   context 'when registering a class' do
     # Example of class
     class SortPerformer
-      def initialize object, result = nil
-        @object = object; @result = result
+      def initialize object, result = nil, method = nil, args = nil
+        @object = object; @result = result; @method = method, @args = args
       end
       
       def call; @object.sort! end
@@ -159,6 +190,60 @@ describe Proxy do
       proxy.reverse.should == [3, 2, 1]
     end
     
+    it 'should use a instance of the registered class for callbefore_all' do
+      array = [3, 2, 1]
+      proxy = proxy_for array, :before_all => SortPerformer
+
+      proxy.should_not be_nil                  
+      proxy.reverse.should == [3, 2, 1]
+    end
+    
+    it 'should use a instance of the registered class for callafter_all' do
+      array = [1, 2, 3]
+      proxy = proxy_for array, :after_all => SortPerformer
+
+      proxy.should_not be_nil                  
+      proxy.reverse.should == [1, 2, 3]
+    end
+    
+  end
+  
+  context 'with dinamic methods' do
+    class MyClass
+      attr_accessor :value
+      def method_missing(symbol, *args); @value; end
+    end
+    
+    it 'should allow dinamic methods' do
+      obj = MyClass.new
+      obj.value = "Crazy Value"
+      
+      proxy = proxy_for obj, :allow_dinamic => true, :before => {
+        :crazy_method => lambda {|obj| obj.value = "proxied value!" }
+      }
+                    
+      proxy.should_not be_nil                  
+      proxy.crazy_method.should == "proxied value!"
+    end
+    
+  end
+  
+  context 'avoiding original execution' do
+    class AvoidOriginal
+      attr_accessor :value
+      def do_something; @value = "original" end
+    end
+    
+    it 'should happen' do
+      obj = AvoidOriginal.new
+      proxy = proxy_for obj, :avoid_original_execution => true, :after_all => lambda {|obj, result, method, args|
+        "avoided"
+      }
+      
+      proxy.should_not be_nil
+      proxy.do_something.should == "avoided"
+      proxy.original_object.value.should be_nil
+    end
   end
   
 end
